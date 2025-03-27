bb0a08fc7a616dd78e90c056e270d87407e02d62
kristján O. Ragnarsson <kristjan@ragnarsson.im>
2025-03-27 15:00:45 +0000

    files

---
+++ src/main.c
@@ -0,0 +1,5 @@
+//
+// Created by Kristján Ragnarsson on 27.3.2025.
+//
+
+#include "main.h"
---
+++ src/main.h
@@ -0,0 +1,8 @@
+//
+// Created by Kristján Ragnarsson on 27.3.2025.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#endif //MAIN_H
