832cfb4f28c2c61d1c477f6dc8e2160e2fb915e0
kristján O. Ragnarsson <kristjan@ragnarsson.im>
2025-04-01 14:41:25 +0000

    added message spliting function

--- src/main.c
+++ src/main.c
@@ -34,7 +34,7 @@ int decrypt_chacha20_poly1305(const unsigned char *ciphertext, int len,
 }
 
 void generate_nonce(unsigned char *nonce) {
-    if (getrandom(nonce, size, 0) != NONOCE_SIZE) {
+    if (getrandom(nonce, size, 0) != NONCE_SIZE) {
         perror("getrandom");
         exit(EXIT_FAILURE);
     }
@@ -58,6 +58,46 @@ void combine_message(
     memcpy(*output, nonce, nonce_len);
     memcpy(*output + nonce_len, ciphertext, text_len);
     memcpy(*output + nonce_len + text_len, tag, tag_len);
+}
+
+void split_message(
+    const unsigned char *input, size_t input_len,
+    unsigned char **nonce, unsigned char **ciphertext, size_t *ciphertext_len,
+    unsigned char **tag
+) {
+    if (input_len < NONCE_SIZE + TAG_SIZE) {
+        fprintf(stderr, "Error: Input buffer too small to contain nonce and tag\n");
+        exit(EXIT_FAILURE);
+    }
+
+    *nonce = malloc(NONCE_SIZE);
+    if (!*nonce) {
+        perror("malloc failed");
+        exit(EXIT_FAILURE);
+    }
+    memcpy(*nonce, input, NONCE_SIZE);
+
+
+    *ciphertext_len = input_len - (NONCE_SIZE + TAG_SIZE);
+
+
+    *ciphertext = malloc(*ciphertext_len);
+    if (!*ciphertext) {
+        perror("malloc failed");
+        free(*nonce);
+        exit(EXIT_FAILURE);
+    }
+    memcpy(*ciphertext, input + NONCE_SIZE, *ciphertext_len);
+
+
+    *tag = malloc(TAG_SIZE);
+    if (!*tag) {
+        perror("malloc failed");
+        free(*nonce);
+        free(*ciphertext);
+        exit(EXIT_FAILURE);
+    }
+    memcpy(*tag, input + NONCE_SIZE + *ciphertext_len, TAG_SIZE);
 }
 
 
@@ -90,11 +130,32 @@ int main() {
     }
     printf("\n");
 
-    combine_message(nonce, sizeof(nonce), ciphertext, sizeof(plaintext), &message, &message_len);
+    combine_message(nonce, sizeof(nonce), ciphertext, sizeof(plaintext), tag, sizeof(tag), &message, &message_len);
     printf("Message: ");
     for (size_t i = 0; i < message_len; i++) {
       printf("%02x ", message[i]);
     }
+
+    printf("\n");
+    unsigned char *tag_t = NULL;
+    unsigned char *nonce_t = NULL;
+    unsigned char *ciphertext_t = NULL;
+    size_t chipertext_len_t;
+    split_message(message, message_len, &nonce_t, &ciphertext_t, &chipertext_len_t, &tag_t);
+
+    printf("Nonce: ");
+    for (size_t i = 0; i < sizeof(nonce); ++i) {
+        printf("%02x ", nonce[i]);
+    }
+    printf("Ciphertext: ");
+    for (size_t i = 0; i < sizeof(ciphertext); i++) {
+        printf("%02x ", ciphertext[i]);
+    }
+
+    for (size_t i = 0; i < sizeof(tag); i++) {
+        printf("%02x ", tag[i]);
+    }
+    printf("\n");
 
     if (decrypt_chacha20_poly1305(ciphertext, sizeof(ciphertext), decrypted, tag) == 0) {
         printf("Decrypted: %s\n", decrypted);
