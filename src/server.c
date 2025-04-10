947983ede6425f557fccbdc3320e4361a7ea9b72
kristján O. Ragnarsson <kristjan@ragnarsson.im>
2025-04-10 16:02:08 +0000

    removed unused files

--- src/server.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by Kristján Ragnarsson on 1.4.2025.
-//
-
-#include "server.h"
--- src/server.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Kristján Ragnarsson on 1.4.2025.
-//
-
-#ifndef SERVER_H
-#define SERVER_H
-
-#endif //SERVER_H
