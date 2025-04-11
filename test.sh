e17f2974a8d68aee3a67953cc98d75f1b9ec7b4e
kristján O. Ragnarsson <kristjan@ragnarsson.im>
2025-04-11 14:35:19 +0000

    maybe better

--- test.sh
+++ test.sh
@@ -3,28 +3,46 @@
 results=results.txt
 en_bin=ICIS_Encryption
 
-ECHO "$HOSTNAME Performance test"
-ECHO "Raspberry PI Model"
-cat /proc/device-tree/model
+echo "$HOSTNAME Performance test"
 
-ECHO "Perf Statistics"
+if [[ -e "/proc/device-tree/model" ]]; then
+  echo "Raspberry PI Model"
+  cat /proc/device-tree/model
+else
+  echo "Not Raspberry pi"
+fi
+
+echo "Perf Statistics"
+mkdir "results"
+
+echo "Encryption & Decryption"
+for i in {28,56,112,224}; do
+  file="results/$i/$results"
+  echo "Plaintext: $i Byte"
+  echo "Encryption_Decryption" >> "$file"
+  mkdir "results/$i"
+  OUTPUT="$(perf stat ./$en_bin < ./text/28.txt)"
+  $OUTPUT >> "$file"
+  $OUTPUT | grep "task-clock"
+done
 
 
-ECHO "28 Byte Plaintext Encryption and Decryption" >> $results
 
-perf stat ./$en_bin < ./text/28.txt >> $results
+#echo "28 Byte Plaintext Encryption and Decryption" >> $results
 
-
-ECHO "56 Byte Plaintext Encryption and Decryption"
-
-perf stat ./$en_bin < ./text/56.txt
+#perf stat ./$en_bin < ./text/28.txt >> $results
 
 
-ECHO "112 Byte Plaintext Encryption and Decryption"
+#echo "56 Byte Plaintext Encryption and Decryption"
 
-perf stat ./$en_bin < ./text/112.txt
+#perf stat ./$en_bin < ./text/56.txt
 
 
-ECHO "224 Byte Plaintext Encryption and Decryption"
+#echo "112 Byte Plaintext Encryption and Decryption"
 
-perf stat ./$en_bin < ./text/224.txt
+#perf stat ./$en_bin < ./text/112.txt
+
+
+#echo "224 Byte Plaintext Encryption and Decryption"
+
+#perf stat ./$en_bin < ./text/224.txt
