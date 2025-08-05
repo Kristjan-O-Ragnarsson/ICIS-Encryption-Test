3820f99e0b721262e94b4880b6f5b606fe922437
kristján O. Ragnarsson <kristjan@ragnarsson.im>
2025-08-05 14:48:55 +0000

    install script for x86_64 ??

--- install.sh
+++ install.sh
@@ -25,6 +25,19 @@ if [[ $(uname -m) == "aarch64" ]] ; then # raspberry pi 4: aarch64
     --disable-opensslextra
 
   make
+elif [[ $(uname -m) == "x86_64" ]] ; then
+  export CFLAGS="-Os -flto -fvisibility=hidden -DWOLFSSL_USER_SETTINGS_FILE='\"user_settings.h\"'"
+  export LDFLAGS="-flto"
+  ./configure \
+    --enable-static \
+    --disable-shared \
+    --disable-crypttests \
+    --disable-examples \
+    --disable-benchmark \
+    --disable-certgen \
+    --disable-keygen \
+    --disable-opensslextra
+  make
 else
   echo "Figure out how to work on x86_64"
 fi
