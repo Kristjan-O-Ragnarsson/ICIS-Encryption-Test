f4783b835bc9e5e2f1380db58eae43991e88cd78
kristján O. Ragnarsson <kristjan@ragnarsson.im>
2025-08-12 13:51:56 +0000

    created better script for automated testing

--- test.sh
+++ test.sh
@@ -1,48 +1,69 @@
 #!/bin/bash
-
-results=results.txt
+datetime=$(date -Iminutes)
+results="test-$datetime.txt"
 en_bin=ICIS_Encryption
+nr_runs=7
 
-echo "$HOSTNAME Performance test"
+echo "$results"
 
-if [[ -e "/proc/device-tree/model" ]]; then
-  echo "Raspberry PI Model"
-  cat /proc/device-tree/model
-else
-  echo "Not Raspberry pi"
-fi
+touch "$results"
 
-echo "Perf Statistics"
-#mkdir "results"
+echo "$HOSTNAME Performance test" | tee -a "$results"
 
-echo "Encryption & Decryption"
+uname -svrm | tee -a "$results"
+#
+#if [[ -e "/proc/device-tree/model" ]]; then
+#  echo "Raspberry PI Model"
+#  cat /proc/device-tree/model
+#else
+#  echo "Not Raspberry pi"
+#fi
+
+
+
+echo "Timing test" | tee -a "$results"
 for i in {28,56,112,224}; do
-  #file="./results/$i/$results"
-  echo "Plaintext: $i Byte"
-  #echo "Encryption_Decryption" >> "$file"
-  #mkdir "results/$i"
-  #OUTPUT="$(perf stat ./$en_bin < ./text/28.txt)"
-  perf stat ./$en_bin < "./text/$i.txt"
-  #$OUTPUT >> "$file"
-  #$OUTPUT | grep "task-clock"
+  echo "Timing: $i Byte" | tee -a "$results"
+  sum_func=0
+  sum_rand=0
+  sum_encr=0
+  sum_decr=0
+
+  for j in $(seq 1 "$nr_runs"); do
+    echo "Run $j" | tee -a "$results"
+    OUTPUT=$(./$en_bin < "./text/$i.txt")
+
+    echo "$OUTPUT" | tee -a "$results"
+
+    func=$(echo "$OUTPUT" | cut -d "," -f 1)
+    sum_func=$((sum_func+func))
+    echo "$func" | tee -a "$results"
+    #echo "$sum_func"
+
+    rand=$(echo "$OUTPUT" | cut -d "," -f 2)
+    sum_rand=$((sum_rand+rand))
+    echo "$rand" | tee -a "$results"
+
+
+    encr=$(echo "$OUTPUT" | cut -d "," -f 3)
+    sum_encr=$((sum_encr+encr))
+    echo "$encr" | tee -a "$results"
+
+    decr=$(echo "$OUTPUT" | cut -d "," -f 4)
+    sum_decr=$((sum_decr+decr))
+    echo "$decr" | tee -a "$results"
+
+  done
+
+  avg_func=$((sum_func / nr_runs))
+  avg_rand=$((sum_rand / nr_runs))
+  avg_encr=$((sum_encr / nr_runs))
+  avg_decr=$((sum_decr / nr_runs))
+
+  echo "AVG over $nr_runs runs" | tee -a "$results"
+  echo "functional: $avg_func" | tee -a "$results"
+  echo "random: $avg_rand" | tee -a "$results"
+  echo "encryption: $avg_encr" | tee -a "$results"
+  echo "Decryption: $avg_decr" | tee -a "$results"
+
 done
-
-
-#echo "28 Byte Plaintext Encryption and Decryption" >> $results
-
-#perf stat ./$en_bin < ./text/28.txt >> $results
-
-
-#echo "56 Byte Plaintext Encryption and Decryption"
-
-#perf stat ./$en_bin < ./text/56.txt
-
-
-#echo "112 Byte Plaintext Encryption and Decryption"
-
-#perf stat ./$en_bin < ./text/112.txt
-
-
-#echo "224 Byte Plaintext Encryption and Decryption"
-
-#perf stat ./$en_bin < ./text/224.txt
