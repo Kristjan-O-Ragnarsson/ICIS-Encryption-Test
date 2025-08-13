2b151cacb92002552532bc69980f35c443b972a3
kristján O. Ragnarsson <kristjan@ragnarsson.im>
2025-08-13 14:16:13 +0000

    moved grep

--- mem-test.sh
+++ mem-test.sh
@@ -21,9 +21,9 @@ for i in {28,56,112,224}; do
 
   for j in $(seq 1 "$nr_runs"); do
     echo "Run $j" | tee -a "$results"
-    OUTPUT=$(cat "./text/$i.txt" | /bin/time -v ./$en_bin  > /dev/null | grep Maximum)
+    OUTPUT=$(cat "./text/$i.txt" | /bin/time -v ./$en_bin  > /dev/null )
 
-    echo "$OUTPUT" | tee -a "$results"
+    echo "$OUTPUT" | grep Maximum | tee -a "$results"
 
 #    func=$(echo "$OUTPUT" | cut -d "," -f 1)
 #    sum_func=$((sum_func+func))
