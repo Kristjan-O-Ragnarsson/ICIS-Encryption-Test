61455c92afbe8467ecf5925da843d211958aff72
kristján O. Ragnarsson <kristjan@ragnarsson.im>
2025-08-13 14:18:38 +0000

    stderr to stdout

--- mem-test.sh
+++ mem-test.sh
@@ -21,7 +21,7 @@ for i in {28,56,112,224}; do
 
   for j in $(seq 1 "$nr_runs"); do
     echo "Run $j" | tee -a "$results"
-    OUTPUT=$(cat "./text/$i.txt" | /bin/time -v ./$en_bin  > /dev/null )
+    OUTPUT=$(cat "./text/$i.txt" | /bin/time -v ./$en_bin  > /dev/null 2>&1)
 
     echo "$OUTPUT" | grep Maximum | tee -a "$results"
 
